eabd47092f8419bfb1b4c8046788efe52a19d628
Lucy Wall <lhwall@gmail.com>
2018-05-22 16:47:30 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,10 @@
 class Actor < ActiveRecord::Base
-  has_many :characters 
+  has_many :characters
   has_many :shows, through: :characters
 
-  
+  def full_name
+    full_name = "#{self.first_name} #{self.last_name}"
+    full_name
+  end 
+
 end
