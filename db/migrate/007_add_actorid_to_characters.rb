eb5a6490d029c115888c4d1efce65aa54c67fe10
Lucy Wall <lhwall@gmail.com>
2018-05-22 17:18:14 +0000

    automatically backed up by learn

--- db/migrate/007_add_actorid_to_characters.rb
+++ db/migrate/007_add_actorid_to_characters.rb
@@ -1,6 +1,6 @@
-class AddCatchphraseToCharacters < ActiveRecord::Migration[4.2]
+class AddActorIdToCharacters < ActiveRecord::Migration[4.2]
 
   def change
-    add_column :characters, :catchphrase, :string
+    add_column :characters, :actor_id, :integer
     end
 end
