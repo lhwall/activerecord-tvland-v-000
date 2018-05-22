7aac22ae21496a7367cea4694f00164d819102a0
Lucy Wall <lhwall@gmail.com>
2018-05-22 16:53:34 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,4 +1,4 @@
-class CreateCharacterss < ActiveRecord::Migration
+class CreateCharacters < ActiveRecord::Migration
 
   def change
     create_table :characters do |t|
--- db/migrate/006_add_catchphrase_to_actors.rb
+++ db/migrate/006_add_catchphrase_to_actors.rb
@@ -1,4 +1,4 @@
-class CreateCharacterss < ActiveRecord::Migration
+class CreateCharacters < ActiveRecord::Migration
 
   def change
     add_column :characters, :catchphrase, :string
